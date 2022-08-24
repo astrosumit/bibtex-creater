e346970616dacbb3b8b76cbc015d6e5d2bfc7ab9
Sumit Jaiswal <32801912+astrosumit@users.noreply.github.com>
2022-08-24 11:47:36 +0800

    Update bibtex.py

--- bibtex.py
+++ bibtex.py
@@ -16,10 +16,24 @@ and make a new one.
 2004ApJ...604L..33Z
 1980Natur.287..307B
 2002A&A...388..470D
+1964Thesis.....000C
 '''
 
 import sys
 import os
+
+#Check the Internet connection with timeout of 10 seconds:
+import requests
+def connected_to_internet(url='https://ui.adsabs.harvard.edu', timeout=10):
+    try:
+        _ = requests.get(url, timeout=timeout)
+        return True
+    except requests.ConnectionError:
+        print("No internet connection available.")
+    return False
+
+if connected_to_internet()==False:
+    sys.exit(3)
 
 # Check the input file exists or not.
 if not os.path.isfile('references'):
@@ -29,39 +43,68 @@ if not os.path.isfile('references'):
 # Delete the previous bibtex "reference.bib" file.
 os.system('rm -rf cc dd? ee? references.bib')
 
+#Additional references not on ADS:
+cite1='1964Thesis.....000C'
+ref1=r"""@phdthesis{1964Thesis.....000C,
+  author       = {Clarke, M.},
+  title        = {PhD thesis},
+  year         = 1964,
+  address      = {Cambridge University},
+}
+
+"""
+cite2='1989QSO...M...0000H'
+ref2=r"""@article1{1989QSO...M...0000H,
+   author = {Hewitt, A. and Burbidge, G.},
+    title = "{A New Optical Catalog of Quasi-Stellar Objects}",
+     year = 1989,
+  refcode = {1989QSO...M...0000H}
+      url = {http://ned.ipac.caltech.edu/uri/NED::Refcode/1989QSO...M...0000H},
+}
+
+"""
+
 # Delete the repeated lines from the input reference file 
 # and Sort of the references on the basis of year.
 os.system("sort references | uniq > cc")
+#remove empty lines:
+os.system("sed -i \'/^$/d\' cc")
 
-'''
-# Sort of the references on the basis of year.
-os.system("cut -c1-4 cc > cc1")
-os.system("paste cc cc1 > cc2")
-os.system("sort -n -k2 cc2 > cc3")
-os.system("cut -f1 cc3 > cc4")
-'''
 
 # Now sorting with respect to the authors' names.
 # Open an input file in the reading mode.
 infile = open('cc', 'r')
+outfile = open('dd1', 'w')
 
 import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+from bs4 import BeautifulSoup as bp
 
 for line in infile.readlines():
     # Make the url and remove the default 'Enter' from each line in infile_name.
-    url = 'http://cdsads.u-strasbg.fr/cgi-bin/nph-bib_query?bibcode=' + \
-        line[0:(len(line)-1)]+'&data_type=BIBTEX&db_key=AST%26nocookieset=1'
+    url = 'https://ui.adsabs.harvard.edu/abs/' + line[0:(len(line)-1)]+'/exportcitation'
     # print(url)
-    # Get the output from the url.
-    url_output = urllib3.PoolManager().request('GET', url).data.decode('utf-8')
-    # print(url_output)
-    # Open an output file for the writing in append mode.
-    outfile = open('dd1', 'a+')
-    for x in url_output:
-        outfile.write(x)
+    #Get the html output from the url.
+    page_html = urllib3.PoolManager().request('GET', url).data.decode('utf-8')
+    #Parse (i.e analyze) the html page into its parts:
+    page_parse=bp(page_html,"html.parser")
+    #find the location of bibtex entry:
+    containers=page_parse.find("textarea",{"class":"export-textarea form-control"},{"readonly":""})
+    # print(containers.text)
+    #Open output file for the writing:
+    if containers != None:
+        outfile.write(containers.text)
+    else:
+        if line[0:(len(line)-1)] == cite1:
+            outfile.write(ref1)
+        elif line[0:(len(line)-1)] == cite2:
+            outfile.write(ref2)
+        elif line[0:(len(line)-1)] == cite3:
+            outfile.write(ref3)
+        elif line[0:(len(line)-1)] == cite4:
+            outfile.write(ref4)
 
-    outfile.close()
-
+outfile.close()
 infile.close()
 
 os.system("grep '@' dd1 > dd2")
@@ -70,36 +113,38 @@ os.system("sed '/ /s//~/g' dd3 > dd4")
 os.system("sed '/.and/s//./g' dd4 > dd5")
 os.system("paste dd2 dd5 > dd6")
 os.system("sort -k2 dd6 > dd7")
-os.system("sed -e 's/@INPROCEEDINGS{//; s/@INBOOK{//; s/@ARTICLE{//' ./dd7 > dd8")
+os.system("sed -e 's/@INPROCEEDINGS{//; s/@INBOOK{//; s/@BOOK{//; s/@PHDTHESIS{//; s/@phdthesis{//; s/@ARTICLE{//; s/@article1{//' ./dd7 > dd8")
 os.system("cut -c1-19 dd8 > dd9")
 
 
 # After sorting we can make the final bibtex file.
 infile = open('dd9', 'r')
+outfile = open('ee1', 'w')
 for line in infile.readlines():
     print("Fetching the reference: "+line)
-    # Make the url and remove the default 'Enter' from each line in infile_name.
-    url = 'http://cdsads.u-strasbg.fr/cgi-bin/nph-bib_query?bibcode=' + \
-        line[0:(len(line)-1)]+'&data_type=BIBTEX&db_key=AST%26nocookieset=1'
-    # print(url)
-    # Get the output from the url.
-    url_output = urllib3.PoolManager().request('GET', url).data.decode('utf-8')
-    # print(url_output)
-    # Open an output file for the writing in append mode.
-    outfile = open('ee1', 'a+')
-    for x in url_output:
-        outfile.write(x)
+    url = 'https://ui.adsabs.harvard.edu/abs/' + line[0:(len(line)-1)]+'/exportcitation'
+    page_html = urllib3.PoolManager().request('GET', url).data.decode('utf-8')
+    page_parse=bp(page_html,"html.parser")
+    containers=page_parse.find("textarea",{"class":"export-textarea form-control"},{"readonly":""})
+    if containers != None:
+        outfile.write(containers.text)
+    else:
+        if line[0:(len(line)-1)] == cite1:
+            outfile.write(ref1)
+        elif line[0:(len(line)-1)] == cite2:
+            outfile.write(ref2)
+        elif line[0:(len(line)-1)] == cite3:
+            outfile.write(ref3)
+        elif line[0:(len(line)-1)] == cite4:
+            outfile.write(ref4)
 
-    outfile.close()
-
+outfile.close()
 infile.close()
 
 
-os.system("grep -v 'Query Results from the ADS Database' ee1 > ee2")
-os.system("grep -v 'Retrieved 1 abstracts' ee2 > ee3")
 
 # Writing the shortcuts of journals' names.
-file = open('ee4', 'w')
+file = open('ee2', 'w')
 file.write("""%@string{mnras = 'MNRAS'}
 %@string{solphys='Sol.~Phys.'}
 %@string{jgr='J.~Geophys.~Res.'}
@@ -117,8 +162,8 @@ file.write("""%@string{mnras = 'MNRAS'}
 %@string{ssr='Space Science Reviews'}
 %@string{aj='Astronomical Journal'}
 
-============================================""")
+============================================\n""")
 file.close()
 
-os.system("cat ee4 ee3 > references.bib")
+os.system("cat ee2 ee1 > references.bib")
 os.system('rm -rf cc dd? ee?')
